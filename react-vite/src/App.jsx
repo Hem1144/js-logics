299f19b799ab63b767a9db31d2bc6db41fe468f9
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-13 22:30:34 +0545

    feat: Set-up the debounding functionality in react

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+
+// const debounce = (fn, delay = 1000) => {
+//   let timer;
+//   return (...args) => {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       fn(...args);
+//     }, delay);
+//   };
+// };
+
+// const makeRequest = (value) => {
+//   console.log("Sending request:", value);
+// };
+
+// const debounced = debounce(makeRequest, 1000);
+
 const App = () => {
-  return <div>Hello there</div>;
+  console.log("re-render");
+  const [value, setValue] = useState();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    // debounced(e.target.value);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("Calling server API", value);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [value]);
+
+  return (
+    <>
+      <h1>Welcome to JS</h1>
+      <input type="text" onChange={handleChange} placeholder="search" />
+      <h2>{value}</h2>
+    </>
+  );
 };
 
 export default App;
